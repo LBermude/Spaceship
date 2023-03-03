3de8663b0dee3aed9eae2ff3c7d48e5d81d5eedd
LBermude <lionel.bermude@cfanumeriqueoutremer.fr>
2023-03-03 10:35:30 -0400

    Commenting

--- SpaceshipControls.js
+++ SpaceshipControls.js
@@ -2,6 +2,8 @@ const lane = document.querySelectorAll('td');
 var currentIndex = 0;
 lane[currentIndex].innerHTML = '<object class="spaceship" type="image/svg+xml" data="spaceship.svg"></object>';
 //Contrôles du vaisseau//
+
+//Mouvement vers la gauche//
 document.addEventListener('keydown', event => {
   if (event.key == 'ArrowLeft') {
     if (currentIndex > 0) {
@@ -9,6 +11,7 @@ document.addEventListener('keydown', event => {
       currentIndex--;
       lane[currentIndex].innerHTML = '<object class="spaceship" type="image/svg+xml" data="spaceship.svg"></object>';
     }
+//Mouvement vers la droite//
   } else if (event.key == 'ArrowRight') {
     if (currentIndex < lane.length - 1) {
       lane[currentIndex].innerHTML = '';
