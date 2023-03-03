a21406cfcc04afb4ba897b1f7c6fd41ab4ee2251
LBermude <lionel.bermude@cfanumeriqueoutremer.fr>
2023-03-03 10:04:10 -0400

    formatting and clean up

---
+++ SpaceshipControls.js
@@ -0,0 +1,19 @@
+const lane = document.querySelectorAll('td');
+var currentIndex = 0;
+lane[currentIndex].innerHTML = '<object class="spaceship" type="image/svg+xml" data="spaceship.svg"></object>';
+//Contrôles du vaisseau//
+document.addEventListener('keydown', event => {
+  if (event.key == 'ArrowLeft') {
+    if (currentIndex > 0) {
+      lane[currentIndex].innerHTML = '';
+      currentIndex--;
+      lane[currentIndex].innerHTML = '<object class="spaceship" type="image/svg+xml" data="spaceship.svg"></object>';
+    }
+  } else if (event.key == 'ArrowRight') {
+    if (currentIndex < lane.length - 1) {
+      lane[currentIndex].innerHTML = '';
+      currentIndex++;
+      lane[currentIndex].innerHTML = '<object class="spaceship" type="image/svg+xml" data="spaceship.svg"></object>';
+    }
+  }
+});
